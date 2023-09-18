7dada5a0ad4275ce870b855d30bddb7e41d92642
Viktor Chernikov <viktor.chernikov@onet.pl>
2023-09-18 17:10:17 +0200

    2023.06 SG 01 - aplikacja konsolowa

---
+++ 2023/czerwiec/01/rozwiazanie/konsola/eratostenes.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+const int n = 100;
+
+void fill(int* nums)
+{
+    for (int i = 0; i < n - 1; ++i)
+    {
+        nums[i] = i + 2;
+    }
+}
+int main()
+{
+    int nums[n - 1];
+    int sqrtn = sqrt(n);
+
+    fill(nums);
+
+    std::cout << "Liczby pierwsze w przedziale 2.." << n << " (n = " << n << "):\n";
+    for (int i = 2; i <= sqrtn; ++i)
+    {
+        if (nums[i - 2] != 0)
+        {
+            std::cout << nums[i - 2] << ", ";
+            for (int j = 2; i * j <= n; ++j)
+            {
+                nums[i * j - 2] = 0;
+            }
+        }
+    }
+    if (nums[sqrtn - 1] != 0)
+    {
+        std::cout << nums[sqrtn - 1];
+    }
+    for (int k = sqrtn; k < n - 1; ++k)
+    {
+        if (nums[k] != 0)
+        {
+            std::cout << ", " << nums[k];
+        }
+    }
+
+    return 0;
+}
